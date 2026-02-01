da3e458d6a7b2f7f7767d541fe22f0c22d6f55d9
P0ffin22 <spkumar0122@gmail.com>
2026-01-31 21:53:39 -0500

    Added Picture for Bitcoin Expo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import project1 from "../assets/projects/AlgorandHackathonPic.jpg";
 import project2 from "../assets/projects/MavenRealtyPic.jpg";
 import project3 from "../assets/projects/PortfolioScreenshot.png";
-import project4 from "../assets/projects/project-4.jpg";
+import project4 from "../assets/projects/MIT2026BitcoinExpoScreenshot.png";
 
 export const HERO_CONTENT = `I’m a freshman middle infielder who truly loves baseball and shows it through my hard work. Baseball is my favorite sport, and I take pride in putting in the extra time, whether it's early practice, late reps, or just getting better to help my team win games. I focus on clean fundamentals, consistent effort, and staying focused even when no one’s watching. My dream is to one day become an MLB player for the New York Mets.`
 export const ABOUT_TEXT = `I’m a dedicated baseball player who puts rSep-13-2025_BaseballlPiceal effort into staying ready for the season. I work out four times a week to stay strong, quick, and prepared to compete. Outside of baseball, I enjoy spending time with friends and family, especially fishing in the summer. I take my academics seriously and keep my grades high because I know discipline in the classroom helps me stay on the field and succeed in life. At home, I focus on eating well, staying on top of my schoolwork, and relaxing with friends. I also challenge myself with 100 pushups and 40 pullups every day to keep my body strong and ready for the demands of the game.`;
